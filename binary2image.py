8dc304df2740c8f465d7e4ccf585d94a472ea7ad
necmettin çarkacı <necmettin.carkaci@gmail.com>
2017-01-12 20:17:29 +0200

    Multi-threading added.

--- binary2image.py
+++ binary2image.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 #
 # Binary to Image Converter
 # Read executable binary files and convert them RGB and greyscale png images
@@ -6,10 +7,12 @@
 # Author: Necmettin Çarkacı
 # E-mail: necmettin [ . ] carkaci [ @ ] gmail [ . ] com
 #
-#Usage : binary2image.py /home/user/binaryDir
+#Usage : binary2image.py /home/user/binaryDir outputdir width
 
 
 import sys, os, math, Image
+from Queue import Queue
+from threading import Thread, current_thread
 
 def getBinaryData(filename):
 	
@@ -40,7 +43,44 @@ def createGreyScaleImage(dataSet,outputfilename):
 
 	image.putdata(dataSet)
 
-	imagename = outputfilename+"_greyscale.png" 
+	imagename = outputfilename+".png" 
+	image.save(imagename)
+	#image.show() 		
+	print imagename+" Greyscale image created"
+
+def createGreyScaleImageSpecificWith(dataSet,outputfilename,width=0):
+	'''
+		Create greyscale image from binary data
+		@ source Malware images: visualization and automatic classification by L. Nataraj
+		@ url http://dl.acm.org/citation.cfm?id=2016908
+	'''
+	if (width == 0): # don't specified
+		size = len(dataSet)
+
+		if (size < 10240) :
+			width = 32
+		elif (10240 <= size <= 10240*3 ):
+			width = 64
+		elif (10240*3 <= size <= 10240*6 ):
+			width = 128	
+		elif (10240*6 <= size <= 10240*10 ):
+			width = 256
+		elif (10240*10 <= size <= 10240*20 ):
+			width = 384
+		elif (10240*20 <= size <= 10240*50 ):
+			width = 512
+		elif (10240*50 <= size <= 10240*100 ):
+			width = 768
+		else :
+			width = 1024
+
+	height = int(size/width)+1
+
+	image = Image.new('L', (width,height))
+
+	image.putdata(dataSet)
+
+	imagename = outputfilename+".png" 
 	image.save(imagename)
 	#image.show() 		
 	print imagename+" Greyscale image created"
@@ -95,17 +135,46 @@ def clean(directory, fileExt):
 				filepath = os.path.join(root, filename)
 				os.remove(filepath)
 
-if __name__ == '__main__':
-	
-	directory = sys.argv[1]
-	filepaths = getFilepaths(directory)
-	
-	for filename in filepaths:
-		print filename	 
-		binaryData = getBinaryData(filename)
-		createGreyScaleImage(binaryData,filename)
-		rgbData    = createRGBDataSet(binaryData)
-		createRGBImage(rgbData,filename)
+def run(fileQueue,outDirectory,width):
+	while not fileQueue.empty():
+		sourceFilename  = fileQueue.get()
+		path, name 	= os.path.split(sourceFilename)
+		name		= name.split('.')[0]
 
+		outputDir	= os.getcwd()+os.sep+outDirectory+os.sep
+		outputFilename	= outputDir+name
+
+		if not os.path.exists(outputDir):
+			os.makedirs(outputDir)
+
+		binaryData = getBinaryData(sourceFilename)
+		createGreyScaleImageSpecificWith(binaryData,outputFilename,width)
+		#createGreyScaleImage(binaryData,outputFilename)
+		#rgbData    = createRGBDataSet(binaryData)
+		#createRGBImage(rgbData,outputFilename)
+		#print (current_thread())
+		fileQueue.task_done()
+
+
+if __name__ == '__main__':
+
+	width 		= 0	
+	inputDirectory 	= sys.argv[1]
+	outDirectory 	= sys.argv[2]
+	width 		= int(sys.argv[3])
+
+	filepaths = getFilepaths(inputDirectory)
+
+	fileQueue = Queue()
+
+	for sourceFilename in filepaths:
+		fileQueue.put(sourceFilename)
+
+	for i in range(7):
+		t = Thread(target = run, args = (fileQueue,outDirectory,width))
+		t.daemon = True
+		t.start()
+
+	fileQueue.join()
 	#clean(sys.argv[1],".png")
 
